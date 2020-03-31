7661c4f30bca0a62d74d62d5c8abb0d6f44e6b90
Jason Prince <jasoncprince@gmail.com>
2020-03-31 17:24:30 -0600

    get request is working

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-const app = express();
+const router = express.Router();
 
-app.use(express.json());
-
-app.listen(process.env.PORT || "3000", () => {
-  console.log(`Server is running on port: ${process.env.PORT || "3000"}`);
+router.get("/", (req, res, next) => {
+  res.json({ test: "test" });
 });
+
+module.exports = router;
